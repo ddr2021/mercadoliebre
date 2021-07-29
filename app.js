0e03b8edfff3768d2fccba5fe7c4188bca176b42
ddr2021 <dannieldavvid2004@gmail.com>
2021-07-29 04:02:03 -0300

    Nav moved to main

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 ﻿//EXPRESS
-const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
 
 //SERVIDOR
 const port = 7070;
-app.listen(port, ()=>{console.log(`Servidor activo en http://www.localhost:${port}`)});
+app.listen(port, () => { console.log(`Servidor activo en http://www.localhost:${port}`) });
 
 //RUTAS ESTATICAS PARA LINKEAR ARCHIVOS DEL PROYECTO
 app.use(express.static('public'));
 app.use(express.static('views'));
 
 //PETICIONES Y RESPUESTAS AL SERVIDOR
-const path= require('path');
-app.get('/', (req, res)=>{res.sendFile(path.resolve(__dirname, 'views', 'home.html' ))});
+const path = require('path');
+app.get('/', (req, res) => { res.sendFile(path.resolve(__dirname, 'views', 'home.html')) });
 
 
